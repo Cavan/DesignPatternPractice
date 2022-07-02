7569e3b7e007c4a82901d459f728ecab021b4aa6
Cavan <cavan.biggs@circumference.ca>
2022-07-02 17:30:35 -0400

    Add project files.

---
+++ AbstractFactory/Patterns/Creational/AbstractFactory.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternImplementation.Patterns.Creational
+{
+    // Gun Factory
+    /// <summary>
+    /// The 'AbstractFactory' abstract class
+    /// </summary>
+    abstract class GunFactory
+    {
+        public abstract HandGun CreateHandGun();
+        public abstract AssaultRifle CreateAssaultRifle();
+
+        public abstract Target CreateTargets();
+    }
+
+
+    /// <summary>
+    /// The 'ConcreteFactory1' class 
+    /// </summary>
+    class GunCachePlayer : GunFactory
+    {
+        public override AssaultRifle CreateAssaultRifle()
+        {
+            return new AK47();
+        }
+
+        public override HandGun CreateHandGun()
+        {
+            return new RemingtonMagnum();
+        }
+
+        public override Target CreateTargets()
+        {
+            return new Enemy();
+        }
+
+    }
+
+    /// <summary>
+    /// The 'ConcreteFactory2' class
+    /// </summary>
+    class GunCacheEnemy : GunFactory
+    {
+        public override AssaultRifle CreateAssaultRifle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override HandGun CreateHandGun()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Target CreateTargets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// The 'AbstractProductA' abstract class
+    /// </summary>
+    abstract class HandGun
+    {
+        public abstract void Shoot(Target t);
+    }
+
+    /// <summary>
+    /// The 'AbstractProductB' abstract class
+    /// </summary>
+    abstract class AssaultRifle
+    {
+        public abstract void Shoot(Target t);
+    }
+
+
+    abstract class Target
+    {
+
+    }
+
+    class RemingtonMagnum : HandGun
+    {
+        public override void Shoot(Target t)
+        {
+            // Shoot gun
+            Console.WriteLine(this.GetType().Name +
+                " Shoots at " + t.GetType().Name);
+        }
+    }
+
+    class AK47 : AssaultRifle
+    {
+        public override void Shoot(Target t)
+        {
+            // Shoot Rifle
+            Console.WriteLine(this.GetType().Name +
+                " Shoots at " + t.GetType().Name);
+        }
+    }
+
+   
+    class Enemy : Target
+    {
+    }
+
+   
+
+   
+
+    class TheBattlefield
+    {
+        private AssaultRifle _assaultRifle;
+        private HandGun _handGun;
+        private Target _target;
+        
+
+        public TheBattlefield(GunFactory factory)
+        {
+            _assaultRifle = factory.CreateAssaultRifle();
+            _handGun = factory.CreateHandGun();
+            _target = factory.CreateTargets();
+        }
+
+        public void RunBattle()
+        {
+            _assaultRifle.Shoot(_target);
+            _handGun.Shoot(_target);
+        }
+    }
+
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,71 @@
+﻿using PatternImplementation.Patterns;
+using PatternImplementation.Patterns.Creational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunAbstractFactory();
+            //RunVehicleBuilder();
+            RunDocumentFactory();
+
+            // Wait for input
+            Console.ReadKey();
+        }
+
+        private static void RunAbstractFactory()
+        {
+            // Create and run the Battlefield world
+            GunFactory playerWeapons = new GunCachePlayer();
+            TheBattlefield world = new TheBattlefield(playerWeapons);
+            world.RunBattle();
+        }
+
+        private static void RunVehicleBuilder()
+        {
+            VehicleBuilder builder;
+
+            // Create shop with vehicle builders
+            Shop shop = new Shop();
+
+            // Construct and display vehicles
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+        }
+
+        private static void RunDocumentFactory()
+        {
+            // Note: constructors call Factory Method
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            // display document pages
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+        }
+    }
+}
