13f6876ba8b38d18b4cb4babf50050776c88c077
Cavan <cavan.biggs@circumference.ca>
2022-07-03 01:22:09 -0400

    Working on structural patterns

---
+++ AbstractFactory/Patterns/Creational/Prototype.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternImplementation.Patterns.Creational
+{
+    /// <summary>
+    /// Prototype Design Pattern
+    /// </summary>
+   
+
+    /// <summary>
+    /// The 'Prototype' abstract class
+    /// </summary>
+    public abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+    
+    /// <summary>
+    /// The 'ConcretePrototype' class
+    /// </summary>
+    public class Color : ColorPrototype
+    {
+        int red;
+        int green;
+        int blue;
+
+        // Constructor
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        // Create a shallow copy
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine(
+                "Cloning color RGB: {0,3}, {1,3}, {2,3}", 
+                red, green, blue);
+
+            return this.MemberwiseClone() as ColorPrototype;
+        }
+    }
+
+    /// <summary>
+    /// Prototype manager
+    /// </summary>
+    public class ColorManager
+    {
+        private Dictionary<string, ColorPrototype> colors =
+            new Dictionary<string, ColorPrototype>();
+        
+        public ColorPrototype this[string key]
+        {
+            get { return colors[key]; }
+            set { colors.Add(key, value); }
+        }
+    
+    }
+
+
+
+}
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,10 +1,11 @@
-﻿using PatternImplementation.Patterns;
-using PatternImplementation.Patterns.Creational;
+﻿using PatternImplementation.Patterns.Creational;
+using PatternImplementation.Patterns;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PatternImplementation.Patterns.Structural;
 
 namespace PatternImplementation
 {
@@ -12,9 +13,33 @@ namespace PatternImplementation
     {
         static void Main(string[] args)
         {
-            //RunAbstractFactory();
-            //RunVehicleBuilder();
-            RunDocumentFactory();
+            int runMethodChoice = 6;
+
+            switch (runMethodChoice)
+            {
+                case 1:
+                    RunAbstractFactory();
+                    break;
+                case 2:
+                    RunVehicleBuilder();
+                    break;
+                case 3:
+                    RunDocumentFactory();
+                    break;
+                case 4:
+                    RunColorManager();
+                    break;
+                case 5:
+                    RunLoadBalancer();
+                    break;
+                case 6:
+                    RunCompoundAdapter();
+                    break;
+
+            default:
+                    break;
+            }
+            
 
             // Wait for input
             Console.ReadKey();
@@ -66,6 +91,67 @@ namespace PatternImplementation
                     Console.WriteLine(" " + page.GetType().Name);
                 }
             }
+        }
+
+        private static void RunColorManager()
+        {
+            ColorManager colormanager = new ColorManager();
+
+            // Initialize with standard colors
+            colormanager["red"] = new Color(255, 0, 0);
+            colormanager["green"] = new Color(0, 255, 0);
+            colormanager["blue"] = new Color(0, 0, 255);
+
+            // User adds personalized colors
+            colormanager["angry"] = new Color(255, 54, 0);
+            colormanager["peace"] = new Color(128, 211, 128);
+            colormanager["flame"] = new Color(211, 34, 20);
+
+            // User clones selected colors 
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
+
+        }
+
+        private static void RunLoadBalancer()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 30 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 30; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+        }
+
+       private static void RunCompoundAdapter()
+        {
+            // Non-adapted chemical compound
+            Compound unknown = new Compound();
+            unknown.Display();
+
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
         }
     }
 }
